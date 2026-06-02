fdf2aa8077ef066b6d0102197712f96de31be2d2
김민지 <juniorandme12@gmail.com>
2026-06-02 15:42:51 +0900

    feat: 학생 수 조회 기능 추가

--- studentManager/src/student/StudentManager.java
+++ studentManager/src/student/StudentManager.java
@@ -21,8 +21,14 @@ public class StudentManager {
         }
         students.remove(name);
     }
+    
     //학생 조회(boolean타입 반환)
     public boolean hasStudent(String name) {
         return students.contains(name);
+    }
+    
+    //학생 수 조회 
+    public int getStudentCount() {
+        return students.size();
     }
 }
